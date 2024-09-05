b90ce40644bc89c2dffcee18e38cfa4bd3722c08
노현주 <dev.hyeonju@gmail.com>
2024-09-05 14:32:54 +0900

    update :: 보낸 메세지 DB에 저장

--- src/main/kotlin/com/ani/ws/global/handler/WebSocketHandler.kt
+++ src/main/kotlin/com/ani/ws/global/handler/WebSocketHandler.kt
@@ -2,6 +2,8 @@ package com.ani.ws.global.handler
 
 import com.ani.ws.common.data.ChatRequest
 import com.ani.ws.common.data.ChatResponse
+import com.ani.ws.domain.repository.Chat
+import com.ani.ws.domain.repository.ChatRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
@@ -13,6 +15,7 @@ import java.time.LocalDateTime
 @Component
 class WebSocketHandler(
     private val objectMapper: ObjectMapper,
+    private val chatRepository: ChatRepository,
     private val sessions: MutableSet<WebSocketSession> = HashSet(),
 ) : TextWebSocketHandler() {
     override fun afterConnectionEstablished(session: WebSocketSession) {
@@ -27,6 +30,8 @@ class WebSocketHandler(
         val chatRequest = objectMapper.readValue(message.payload, ChatRequest::class.java)
 
         sessions.map { unit -> unit.sendMessage(chatRequest) }
+        chatRequest.save()
+
     }
 
     private fun WebSocketSession.sendMessage(chatRequest: ChatRequest) {
@@ -40,5 +45,14 @@ class WebSocketHandler(
 
         TextMessage(objectMapper.writeValueAsString(chatResponse))
             .run(this::sendMessage)
+    }
+
+    private fun ChatRequest.save() {
+        chatRepository.save(
+            Chat(
+                name = name,
+                message = message
+            )
+        )
     }
 }
