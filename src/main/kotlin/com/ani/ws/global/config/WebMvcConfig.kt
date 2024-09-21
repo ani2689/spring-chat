e7d864d77e5796179f376d6297a994b5029a3ea8
노현주 <dev.hyeonju@gmail.com>
2024-09-21 22:21:38 +0900

    fix :: cors

---
+++ src/main/kotlin/com/ani/ws/global/config/WebMvcConfig.kt
@@ -0,0 +1,18 @@
+package com.ani.ws.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+    }
+}
